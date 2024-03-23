68fe1bb24e180728490f34b46793228a3d368839
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-03-23 13:23:06 -0400

    init setup

---
+++ src/main/java/com/venkatesh/SpringJpaAuth/SpringJpaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.venkatesh.SpringJpaAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venkatesh/SpringJpaAuth/SpringJpaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.SpringJpaAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
